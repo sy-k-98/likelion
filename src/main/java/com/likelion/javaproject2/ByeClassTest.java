5c52eb446983be6e3a9d8da77cb73f4b330c8a46
sy-k-98 <sb05220@naver.com>
2023-04-19 11:31:06 +0900

    feat : Bye 기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/com/likelion/javaproject2/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.likelion.javaproject2;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+        ByeClass byeClass = new ByeClass();
+        byeClass.print();
+    }
+}
