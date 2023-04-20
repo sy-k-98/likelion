99c30007acd572c1269889222aa4e5d504cfdea8
sy-k-98 <sb05220@naver.com>
2023-04-20 09:24:32 +0900

    feat : [복습] 숫자 2개 받아서 입력하기 - ScannerEx2, ScannerExTest2 추가

---
+++ src/main/java/week1/ScannerEx2.java
@@ -0,0 +1,12 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ScannerEx2 {
+
+    public void readTwoNumbersAndPlus(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
---
+++ src/main/java/week1/ScannerExTest2.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class ScannerExTest2 {
+
+    public static void main(String[] args) {
+        ScannerEx2 se = new ScannerEx2();
+        se.readTwoNumbersAndPlus();
+    }
+
+}
