810a9b0c4d9aa2780588e5f85e77984a361c7d61
sy-k-98 <sb05220@naver.com>
2023-04-20 14:38:02 +0900

    feat : 변수 선언, 변수를 이용한 더하기 - ByteTypeEx, ScannerFormatting 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/ByteTypeEx.java
@@ -0,0 +1,15 @@
+package com.likelion.javaproject2.week1;
+
+public class ByteTypeEx {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -127;
+        byte byte3 = -128;
+//        byte byte4 = -129; // 안 됨
+//        byte byte5 = 128; // 안 됨
+
+        System.out.println(byte1);
+        System.out.println(byte2);
+        System.out.println(byte3);
+    }
+}
