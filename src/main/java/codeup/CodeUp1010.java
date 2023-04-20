5153143e9ef1f5d009a467bfbbb32214f867f5ca
sy-k-98 <sb05220@naver.com>
2023-04-20 11:36:55 +0900

    feat : CodeUp 1010번 - 1010번 풀기

---
+++ src/main/java/codeup/CodeUp1010.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1010 {
+    public static void main(String[] args) {
+        int n ;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        System.out.println(n);
+    }
+}
