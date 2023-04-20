51ba17cb416f80aad2e6501bae4b2523b72480b8
sy-k-98 <sb05220@naver.com>
2023-04-20 11:50:06 +0900

    feat : PrintfDate 추가 - 날짜 출력

---
+++ src/main/java/com/likelion/javaproject2/week1/PrintfDate.java
@@ -0,0 +1,7 @@
+package com.likelion.javaproject2.week1;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d-%02d-%02d", 2023, 4, 20);
+    }
+}
