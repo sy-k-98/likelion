765c10dd9ecc86d51c914b5e855d5ce469f49611
sy-k-98 <sb05220@naver.com>
2023-04-20 09:40:21 +0900

    feat : 문자열 두개 받아서 합치기 - ScannerStrEx, ScannerStrExTest 추가

---
+++ src/main/java/week1/ScannerStrEx.java
@@ -0,0 +1,11 @@
+package week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+
+    void inputStringAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
---
+++ src/main/java/week1/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package week1;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringAndPlus();
+    }
+}
