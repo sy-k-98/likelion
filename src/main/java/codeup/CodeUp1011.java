986b4637e462a3a5869ee81a2c3d7884d4f384e4
sy-k-98 <sb05220@naver.com>
2023-04-20 13:45:02 +0900

    feat : CodeUp 1011-1013 - 1011번부터 1013번까지 풀기

---
+++ src/main/java/codeup/CodeUp1011.java
@@ -0,0 +1,10 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%s", sc.next());
+    }
+}
