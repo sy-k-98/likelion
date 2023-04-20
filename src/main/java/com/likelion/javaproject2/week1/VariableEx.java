14c7a2b772d8396e66c0812cce02a4e525207dd8
sy-k-98 <sb05220@naver.com>
2023-04-20 15:15:22 +0900

    feat : 변수 선언과 초기화의 구분 - VariableEx 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/VariableEx.java
@@ -0,0 +1,18 @@
+package com.likelion.javaproject2.week1;
+
+import com.likelion.javaproject2.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0; // 최초로 값을 지정하는 것, 초기화
+
+        PrintHello printHello; // 변수 선언
+//        printHello.print(); // 초기화가 안 됨
+        printHello = new PrintHello(); // new를 이용해 인스턴스화 및 초기화
+
+        System.out.println(iVal);
+        printHello.print();
+
+    }
+}
