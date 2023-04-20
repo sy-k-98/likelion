6bb96e4ff3026065934371d30013698ed43c9df9
sy-k-98 <sb05220@naver.com>
2023-04-20 10:42:19 +0900

    feat : print()의 이용 - ScannerWithGuide, ScannerWithGuideTest 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/ScannerWithGuideTest.java
@@ -0,0 +1,11 @@
+package com.likelion.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerWithGuideTest {
+    public static void main(String[] args) {
+        ScannerWithGuide swg = new ScannerWithGuide();
+        swg.inputTwoNumbersAndPlus();
+    }
+
+}
