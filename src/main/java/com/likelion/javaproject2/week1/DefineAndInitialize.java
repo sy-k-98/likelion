37870e0ab7b4fc92bc5bae2f9fbe196770a94f48
sy-k-98 <sb05220@naver.com>
2023-04-20 15:29:56 +0900

    feat : 변수 선언과 초기화를 한번에 하기, 변수에 담았던 값 더하기 - DefineAndInitialize, ScannerAndVariable 추가

---
+++ src/main/java/com/likelion/javaproject2/week1/DefineAndInitialize.java
@@ -0,0 +1,14 @@
+package com.likelion.javaproject2.week1;
+
+import com.likelion.javaproject2.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
