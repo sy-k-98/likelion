656cdf4fac307b03745c7d805d91b3770cbd9b0d
sy-k-98 <sb05220@naver.com>
2023-04-20 13:39:04 +0900

    feat : float 형 출력 - float 출력, 평균 구하기

---
+++ src/main/java/com/likelion/javaproject2/week1/FloatFormatEx.java
@@ -0,0 +1,9 @@
+package com.likelion.javaproject2.week1;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 0.5);
+        System.out.printf("%.1f\n", 1.44);
+        System.out.printf("%.2f\n", 3.78987);
+    }
+}
