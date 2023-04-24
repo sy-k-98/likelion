1d8b747874611af21a76010a91bb76d1f7b607a4
sy-k-98 <sb05220@naver.com>
2023-04-24 15:52:44 +0900

    feat : CodeUp 1021-1037 - 1021, 1022, 1023, 1028, 1029, 1030, 1031, 1032, 1033, 1034, 1035, 1036, 1037번 풀기

---
+++ src/main/java/codeup/CodeUp1034.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(8);
+        System.out.printf("%d", num);
+    }
+}
