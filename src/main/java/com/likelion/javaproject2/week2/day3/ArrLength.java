8a80f1f194ac17eef56c38751b8f77568fc6f5f0
sy-k-98 <sb05220@naver.com>
2023-04-26 14:21:03 +0900

    feat : 배열 길이 구하기, 연산자 - ArrLength : 배열 길이 - ArithmeticOperators1 : 사칙연산 + 나머지 계산하기 - RemainderQuote : 몫과 나머지 구하기

---
+++ src/main/java/com/likelion/javaproject2/week2/day3/ArrLength.java
@@ -0,0 +1,9 @@
+package com.likelion.javaproject2.week2.day3;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        System.out.println(arr.length);
+    }
+}
