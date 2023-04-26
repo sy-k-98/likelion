4a4a8208d3b6cb5317c09680bc3e60e129916bbd
sy-k-98 <sb05220@naver.com>
2023-04-26 14:19:36 +0900

    feat : CodeUp 1041 - 1046 - 1041부터 1046번 풀기

---
+++ src/main/java/codeup/CodeUp1041.java
@@ -0,0 +1,11 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char chr = sc.next().charAt(0);
+        System.out.println((char)(chr + 1));
+    }
+}
---
+++ src/main/java/codeup/CodeUp1046.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num1 = sc.nextInt();
+        long num2 = sc.nextInt();
+        long num3 = sc.nextInt();
+        long sum = num1 + num2 + num3;
+        double avg = (double)sum / 3;
+        System.out.printf("%d\n%.1f", sum, avg);
+
+    }
+}
