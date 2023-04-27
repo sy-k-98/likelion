a2d7c5df89f4d59b90b70e1cbd6cc7a11d8a30ad
sy-k-98 <sb05220@naver.com>
2023-04-27 11:11:11 +0900

    feat : 비교연산자 - ComparisonArray : 배열의 값 비교 - ComparisonOperatorsEx : 값이 같은지 비교 - ComparisonOperatorsEx : 값끼리 비교

---
+++ src/main/java/com/likelion/javaproject2/week2/day4/ComparisonOperatorsEx.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+        int val1 = 10;
+        int val2 = 20;
+        boolean result = val1 == val2;
+        System.out.printf("val1와 val2가 같은지? %b", result);
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/week2/day4/ComparisonOperatorsEx2.java
@@ -0,0 +1,14 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class ComparisonOperatorsEx2 {
+    public static void main(String[] args) {
+        int val1 = 10;
+        int val2 = 20;
+        System.out.printf("%d == %d --> %b\n", val1, val2, val1 == val2);
+        System.out.printf("%d != %d --> %b\n", val1, val2, val1 != val2);
+        System.out.printf("%d < %d --> %b\n", val1, val2, val1 < val2);
+        System.out.printf("%d <= %d --> %b\n", val1, val2, val1 <= val2);
+        System.out.printf("%d > %d --> %b\n", val1, val2, val1 > val2);
+        System.out.printf("%d >= %d --> %b\n", val1, val2, val1 >= val2);
+    }
+}
