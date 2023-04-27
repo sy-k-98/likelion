b07680279996129564a8e78adeee06f9a1beabd6
sy-k-98 <sb05220@naver.com>
2023-04-27 10:20:32 +0900

    feat : 배열의 합, 복합대입연산자 - SumOfValues : 배열에 있는 값 더하기 - Accumulate687Compound : 누적합을 복합대입연산자로 풀기 - CompoundAssignmentOperator : 복합대입연산자 예시 - IncreaseFirst : 증감연산자 안쓰기 - IncreaseFirst2 : 증감연산자 쓰기

---
+++ src/main/java/com/likelion/javaproject2/week2/day4/IncreaseFirst2.java
@@ -0,0 +1,10 @@
+package com.likelion.javaproject2.week2.day4;
+
+public class IncreaseFirst2 {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 9};
+        System.out.println(arr[idx++]); // 0번째 있는 2를 출력
+        System.out.println(arr[idx++]); // 1번째 있는 1을 출력
+    }
+}
