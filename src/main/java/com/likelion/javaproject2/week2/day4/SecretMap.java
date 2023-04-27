7d517493eb10e73adad30b99d64d8c5cbf4708b5
sy-k-98 <sb05220@naver.com>
2023-04-27 15:52:14 +0900

    feat : 프로그래머스 비밀지도 풀기 - SecretMap : 코드 수정

--- src/main/java/com/likelion/javaproject2/week2/day4/SecretMap.java
+++ src/main/java/com/likelion/javaproject2/week2/day4/SecretMap.java
@@ -8,14 +8,10 @@ public class SecretMap {
 
         for (int i = 0; i < n; i++) {
             answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-        }
-
-        for (int i = 0; i < n; i++) {
             answer[i] = String.format("%"+n+"s", answer[i]);
             answer[i] = answer[i].replaceAll("1", "#");
             answer[i] = answer[i].replaceAll("0", " ");
         }
-
         return answer;
     }
 
