bf72fe320cfd2e4abfce0ab79f63e9b47fc35693
sy-k-98 <sb05220@naver.com>
2023-05-01 13:17:16 +0900

    feat : For문 - ForLoopForwardStep1To10 : 1~10까지 출력 - ForLoopReverse : 10~1까지 출력, 10~0까지 출력 - ForLoopForwardStep : 짝수만 출력, 홀수만 출력

---
+++ src/main/java/com/likelion/javaproject2/week3/day1/ForLoopReverse.java
@@ -0,0 +1,15 @@
+package com.likelion.javaproject2.week3.day1;
+
+public class ForLoopReverse {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.printf("%d ",i);
+        }
+        System.out.println();
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.printf("%d ",i);
+        }
+        System.out.println();
+    }
+}
