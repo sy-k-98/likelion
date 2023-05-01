52f792676ff98d298e758b2f456514f516c27ab8
sy-k-98 <sb05220@naver.com>
2023-05-01 16:14:24 +0900

    feat : CodeUp 1080-1082 - 1080번부터 1082번까지 풀기

---
+++ src/main/java/codeup/CodeUp1082.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1082 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String alphabet = sc.next();
+        int num = Integer.parseInt(alphabet, 16);
+
+        for (int i = 1; i < 16 ; i++) {
+            System.out.printf("%X*%X=%X\n", num, i, num * i);
+        }
+    }
+}
