a613903a164b2d39f1ab82d67a92155d160c22a5
sy-k-98 <sb05220@naver.com>
2023-05-01 10:55:34 +0900

    feat : For문 - ForLoopForward : 0~9까지 출력하기 - ForLoopPrintHorizontal : 0~9까지 가로로 출력하기

---
+++ src/main/java/com/likelion/javaproject2/week3/day1/ForLoopForward.java
@@ -0,0 +1,9 @@
+package com.likelion.javaproject2.week3.day1;
+
+public class ForLoopForward {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i); // i가 0~9까지 변함
+        }
+    }
+}
