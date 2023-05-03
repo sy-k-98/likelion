72e9a2f9733ae860be344b544c66fa0bb4f4ec0e
sy-k-98 <sb05220@naver.com>
2023-05-03 15:51:44 +0900

    feat : CodeUp 1087-1095 - 1087번부터 1095번까지 풀기

---
+++ src/main/java/codeup/CodeUp1087.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1087 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; sum < num; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
