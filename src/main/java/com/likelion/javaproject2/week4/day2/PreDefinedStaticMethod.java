63943ab7d9bda89e7c5a7768b3a86ed05edd5b60
sy-k-98 <sb05220@naver.com>
2023-05-09 11:39:58 +0900

    feat : static 메소드 - Calculator, CalculatorTest : 스태틱 메소드 호출 - PreDefinedStaticMethod : 다른 클래스에서 자주 사용하는 메소드 호출

---
+++ src/main/java/com/likelion/javaproject2/week4/day2/PreDefinedStaticMethod.java
@@ -0,0 +1,8 @@
+package com.likelion.javaproject2.week4.day2;
+
+public class PreDefinedStaticMethod {
+    public static void main(String[] args) {
+        Integer.parseInt("30");
+        Math.pow(2, 10);
+    }
+}
