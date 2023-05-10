33e22328986e3bfa3774935f6825735fe7f99a53
sy-k-98 <sb05220@naver.com>
2023-05-10 14:09:49 +0900

    feat : 인터페이스 실습 - Printer : 인터페이스 - ConsolePrinter : 콘솔로 출력 - FilePrinter : 파일로 출력 - RightTrianglePrinter : 실행

---
+++ src/main/java/com/likelion/javaproject2/week4/day3/ConsolePrinter.java
@@ -0,0 +1,13 @@
+package com.likelion.javaproject2.week4.day3;
+
+import java.io.IOException;
+
+public class ConsolePrinter implements Printer{
+
+    @Override
+    public void print(String[] lines) throws IOException {
+        for (int i = 0; i < lines.length; i++) {
+            System.out.print(lines[i]);
+        }
+    }
+}
---
+++ src/main/java/com/likelion/javaproject2/week4/day3/RightTrianglePrinter.java
@@ -0,0 +1,32 @@
+package com.likelion.javaproject2.week4.day3;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RightTrianglePrinter {
+
+    private Printer printer;
+
+    public RightTrianglePrinter(Printer printer) {
+        this.printer = printer;
+    }
+
+    public String makeALine(int h, int i) {
+        return String.format("%s\n", "*".repeat(i));
+    }
+
+    public void printShape(int h) throws IOException {
+        String[] lines = new String[h];
+
+        for (int i = 0; i < h; i++) {
+            lines[i] = makeALine(h, i + 1);
+        }
+        printer.print(lines);
+    }
+
+    public static void main(String[] args) throws IOException {
+        RightTrianglePrinter rtp = new RightTrianglePrinter(new FilePrinter());
+        rtp.printShape(5);
+    }
+}
